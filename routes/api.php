1ccc1f6ac305602e41c2d81a37c824966da5cde4
putixin <52putixin@gmail.com>
2018-06-28 06:48:23 +0000

    topics show

--- routes/api.php
+++ routes/api.php
@@ -45,8 +45,12 @@ $api->version('v1', [
         // 游客可以访问的接口
         $api->get('categories', 'categoriesController@index')
             ->name('api.categories.index');
+        // 话题列表
         $api->get('topics', 'TopicsController@index')
             ->name('api.topics.index');
+        // 话题详情
+        $api->get('topics/{topic}', 'TopicsController@show')
+            ->name('api.topics.show');
         // 每个用户发布的话题
         $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
