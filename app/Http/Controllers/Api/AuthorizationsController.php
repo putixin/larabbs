c85e1f27973c0aa3b88813cd5b9ead3a5add3a8b
putixin <52putixin@gmail.com>
2018-07-05 06:45:26 +0000

    social login change to Passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -12,9 +12,12 @@ use Zend\Diactoros\Response as Psr7Response;
 use Psr\Http\Message\ServerRequestInterface;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\AuthorizationServer;
+use App\Traits\PassportToken;
 
 class AuthorizationsController extends Controller
 {
+    use PassportToken;
+
     public function store(AuthorizationRequest $request, AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
         try {
@@ -72,8 +75,8 @@ class AuthorizationsController extends Controller
             break;
         }
 
-        $token = Auth::guard('api')->fromUser($user);
-        return $this->respondWithToken($token)->setStatusCode(201);
+        $result = $this->getBearerTokenByUser($user, '1', false);
+        return $this->response->array($result)->setStatusCode(201);
     }
 
     public function update(AuthorizationServer $server, ServerRequestInterface $serverRequest)
